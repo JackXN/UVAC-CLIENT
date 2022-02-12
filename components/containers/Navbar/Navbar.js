af6b6e79de37edb39b64c542cadd70d33cbe6170
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-02-12 06:37:48 -0700

    initial nav

--- components/containers/Navbar/Navbar.js
+++ components/containers/Navbar/Navbar.js
@@ -1,82 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import { IconContext } from "react-icons";
-import { BiMenu, BiX } from "react-icons/bi";
-import {Button} from '../../Globalstyles';
-import 
-{
-    Nav,
-    NavbarContainer,
-    NavLogo,
-    NavIcon,
-    MenuIcon,
-    Menu,
-    MenuItem,
-    MenuLink,
-    MenuItemBtn,
-    MenuLinkBtn,
-} from './Navbar.styles';
-const Navbar = () => {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    const handleClick = () => setClick(!click);
-    const closeMenu = () => setClick(false);
+// Components
+import {Box, StatUpArrow, Text} from '@chakra-ui/react';
 
-    const showButton = () =>{
-        if(window.innerWidth<= 1000){
-            setButton(false);
-        }else{
-            setButton(true);
-        }
-    }
 
-    useEffect(() => {
-        showButton();
-    }, []);
-    window.addEventListener('resize', showButton);
-    
-    return (
-        <div>
-            <IconContext.Provider value={{ color: '#fff'}}>
-                <Nav>
-                    <NavbarContainer>
-                        <NavLogo to="/">
-                            <NavIcon/>
-                            Home Made
-                        </NavLogo>
-                        <MenuIcon onClick={handleClick}>
-                            {click ? <BiX/> : <BiMenu/>}
-                        </MenuIcon>
+// Styles
+import styles from './styles';
 
-                        <Menu onClick={handleClick} click={click}>
-                            <MenuItem>
-                                <MenuLink onClick={closeMenu} to="/">Home</MenuLink>
-                            </MenuItem>
-                            <MenuItem>
-                                <MenuLink onClick={closeMenu} to="/about">About</MenuLink>
-                            </MenuItem>
-                            <MenuItem>
-                                <MenuLink onClick={closeMenu} to="/recipe">Recipes</MenuLink>
-                            </MenuItem>
-                            <MenuItemBtn>
-                                {button?(
-                                    <MenuLinkBtn to="/order-now">
-                                        <Button primary>Order Now</Button>
-                                    </MenuLinkBtn>
-                                ): (
-                                    <MenuLinkBtn to="/order-now">
-                                        <Button primary bigFont onClick={closeMenu}>Order Now</Button>
-                                    </MenuLinkBtn>
-                                )
-                                }
-                            </MenuItemBtn>
-                        </Menu>
-
-                    </NavbarContainer>
-
-                </Nav>
-            </IconContext.Provider>
-        </div>
-    )
+function Navbar() {
+  return (
+   <Box sx={styles.navContainer}>
+     <Box sx={styles.navbarContent}>
+<Box sx={styles.leftContainer}>
+  <Text as='h1'>UVAC <span>ELITE</span></Text>
+    <Text as='h3' position='relative' bottom='20px'>Quality Cleaned Ventilation</Text>
+</Box>
+</Box>
+   </Box>
+  )
 }
 
-export default Navbar;
+
+export default Navbar
---
+++ components/containers/Navbar/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+    navContainer: {
+  display: 'flex',
+  bg: 'orange',
+    },
+    leftContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      padding: '20px',
+      lineHeight: '10px',
+      fontFamily: "Montserrat"
+    }
+  }
+  
+
+
+export default styles;
