6e48bca17a7d98027c1e96dd8c4576fea22696df
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-02-23 00:10:17 -0700

    services section init

--- components/containers/Map/index.js
+++ components/containers/Map/index.js
@@ -1,71 +1,65 @@
 // Components
-import {Box, Text} from '@chakra-ui/react';
-import Image from 'next/image';
-import styles from './styles';
+import { Box, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import styles from "./styles";
 
-
+const colorScheme = [
+  {
+    color: "red",
+    second: "orange",
+    bank: "hold up blue",
+  },
+];
 
 // Icons
-import {AiOutlineSafetyCertificate as Certification} from 'react-icons/ai';
-import {BiSupport as Support} from 'react-icons/bi';
-import {FaRegMoneyBillAlt as Finance} from 'react-icons/fa';
-
+import { AiOutlineSafetyCertificate as Certification } from "react-icons/ai";
+import { BiSupport as Support } from "react-icons/bi";
+import { FaRegMoneyBillAlt as Finance } from "react-icons/fa";
 
 const data = [
-    {
-        label: 'Certified Dealer',
-        description: 'Whether you are looking for quality service or professional and honest inspections, UVAC Elite is certified and up to par with the latest standards',
-        image: 'link'
-    },
-    {
-        label: 'Request A Service Call',
-        description: 'Whether you are looking for quality service or professional and honest inspections, UVAC Elite is certified and up to par with the latest standards',
-        image: 'link'
-    },
-    {
-        label: 'Financing Options',
-        description: 'Whether you are looking for quality service or professional and honest inspections, UVAC Elite is certified and up to par with the latest standards',
-        image: 'link'
-    },
+  {
+    label: "Certified Dealer",
+    description:
+      "Whether you are looking for quality service or professional and honest inspections, UVAC Elite is certified and up to par with the latest standards",
+    isTrue: true,
+    image: [Certification],
+  },
+  {
+    label: "Request A Service Call",
+    description:
+      "Whether you are looking for quality service or professional and honest inspections, UVAC Elite is certified and up to par with the latest standards",
+    image: [Support],
+  },
+  {
+    label: "Financing Options",
+    description:
+      "Whether you are looking for quality service or professional and honest inspections, UVAC Elite is certified and up to par with the latest standards",
+    image: [Finance],
+  },
+];
 
-
-
-]
+const icons = [Support, Certification, Finance];
 
 function index() {
-    return (
-        <>
-        <Box sx={styles.wrapper}>
-        <Text as='h1' sx={styles.title}>
-        Some of the services we provide
-     </Text>
+  return (
+    <>
+      <Box sx={styles.wrapper}>
         <Box sx={styles.container}>
-
-     <Box sx={styles.card}>
-{data.map((item, index) => (
-    <Box key={index}>
-         <Certification/>
-         <Box sx={styles.information}>
-<Text as='h2'>{item.label}</Text>
-<Text as='p'>{item.description}</Text>
-             </Box>
+          <Box sx={styles.card}>
+            {data.map((item, index) => (
+              <Box key={index} sx={styles.cardContainer}>
+                <Box alignSelf="center" sx={styles.icon}>{item.image ? <Support /> : null}</Box>
+                <Box sx={styles.information}>
+                  <Text as="h2">{item.label}</Text>
+                  <Text as="p">{item.description}</Text>
+                </Box>
+              </Box>
+            ))}
+          </Box>
         </Box>
-        
-))}
-
-
-     </Box>
-
-
-
-
-        </Box>
-        </Box>
-        </>
-    )
+      </Box>
+    </>
+  );
 }
 
-
-
-
-export default index
+export default index;
--- components/containers/Map/styles.js
+++ components/containers/Map/styles.js
@@ -1,39 +1,58 @@
 const styles = {
-    container: {
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        textAlign: 'center',
-        background: 'rgb(116,111,111)',
-background: 'linear-gradient(90deg, rgba(116,111,111,1) 0%, rgba(49,48,48,1) 100%)',
-backgroundPosition: 'left',
-flexWrap: 'wrap',
-    },
-    wrapper: {
-        display: 'flex',
-        flexDirection: 'column',
-     
-       
-    },
+  container: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    background: "rgb(116,111,111)",
+    background:
+      "linear-gradient(90deg, rgba(116,111,111,1) 0%, rgba(49,48,48,1) 100%)",
+    backgroundPosition: "left",
+    flexWrap: "wrap",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: 'wrap'
+  },
 
-    title: {
-        fontSize: '20px',
-        color: 'white',
-        alignSelf: 'center'
-    },
-    information: {
+  title: {
+    fontSize: "20px",
+    color: "white",
+    alignSelf: "center",
+  },
+  information: {
+    margin: ["20px"],
 
+    h2: {
+      color: "#ffff",
+      fontSize: "23px",
+      fontFamily: "Montserrat",
+      fontWeight: "bold",
     },
-    card: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: [
-            'column',
-            'row',
-        ]
+  },
+  card: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: ["column", "column", "column", "column", "row"],
+  },
+  cardContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignitems: "center",
+    textAlign: "center",
+    margin: '30px'
+  },
 
-    }
-}
+  icon: {
+      fontSize: '30px',
+      background: '#333131',
+      padding: '15px',
+      borderRadius: '50px',
+      color: '#646464'
+  }
+};
 
 export default styles;
